97205c13ac18e8815db403e4456f2c6c15d8bc4b
RSaliu <ridwanriley@gmail.com>
2024-01-27 00:40:08 +0100

    [wifi-scale] - fix memory issues

--- lib/AWSManager/AWSManager.cpp
+++ lib/AWSManager/AWSManager.cpp
@@ -2,18 +2,19 @@
 #include "AWSManager.h"
 #include "secrets.h"
 #define AWS_TIMOUT_COUNTER 3
-AWSManager::AWSManager(WiFiManager* wifiManager):wifiManager(wifiManager)
+AWSManager::AWSManager(WiFiManager *wifiManager) : wifiManager(wifiManager)
 {
-
 }
 
-AWSErrors AWSManager::init(){
+AWSErrors AWSManager::init()
+{
     Serial.println("in aws init");
-    if(!strlen(AWS_CERT_CA) || !strlen(AWS_CERT_CRT) || !strlen(AWS_CERT_PRIVATE)){
+    if (!strlen(AWS_CERT_CA) || !strlen(AWS_CERT_CRT) || !strlen(AWS_CERT_PRIVATE))
+    {
         Serial.println("one of the credential is bad");
         return AWSErrors::INVALID_CREDENTIALS;
     }
-    if(!strlen(AWS_IOT_ENDPOINT))
+    if (!strlen(AWS_IOT_ENDPOINT))
     {
         Serial.println("bad endpoint");
         return AWSErrors::INVALID_ENDPOINT;
@@ -26,73 +27,81 @@ AWSErrors AWSManager::init(){
     this->client.setServer(AWS_IOT_ENDPOINT, 8883);
 
     // Create a message handler
-    this->client.setCallback(messageHandler); 
+    this->client.setCallback(messageHandler);
     Serial.println("retruning OK");
-    
+
     return AWSErrors::OK;
 }
 AWSErrors AWSManager::connect()
 {
-    if(connected){
+    if (client.connected())
+    {
         Serial.println("client already connected");
         return AWSErrors::OK;
     }
-    if(!wifiManager->isConnected())
+    if (!wifiManager->isConnected())
     {
         Serial.println("wifi not connected");
         return AWSErrors::INVALID_STATE;
     }
 
     uint8_t counter = 0;
-   while (!client.connect(THINGNAME) && counter < AWS_TIMOUT_COUNTER ) {
+    while (!client.connect(THINGNAME) && counter < AWS_TIMOUT_COUNTER)
+    {
         Serial.print(".");
         counter++;
         delay(1000);
     }
-    if(counter >= AWS_TIMOUT_COUNTER){
+    if (counter >= AWS_TIMOUT_COUNTER)
+    {
         Serial.println("aws client timeout");
         return AWSErrors::CONNECTION_FAILED;
-    }else{
+    }
+    else
+    {
         Serial.println("aws client connection ok");
-        connected= true;
         return AWSErrors::OK;
-    }  
-
+    }
 }
-AWSErrors AWSManager::publish(std::string& topic, std::string& payload){
-    if(!connected){
+AWSErrors AWSManager::publish(std::string &topic, std::string payload)
+{
+    if (!client.connected())
+    {
         return AWSErrors::INVALID_STATE;
     }
     uint32_t payload_length = payload.length();
-    bool result = client.beginPublish(topic.c_str(),payload_length,false);
-    if(!result){
+    bool result = client.beginPublish(topic.c_str(), payload_length, false);
+    if (!result)
+    {
         Serial.println("unable to begin publish");
         return AWSErrors::PUBLISH_FAILED;
     }
-    Serial.printf("total payload length is: %d\n",payload_length);
+    Serial.printf("total payload length is: %d\n", payload_length);
     uint32_t counter = 0;
-    uint8_t step_size = 10;
     uint32_t max_index = payload_length;
-    while(counter < max_index){
-        uint32_t end_index = counter+step_size;
-        if(end_index > max_index)
-        {
-            end_index = max_index;
-        }
-        size_t sent = client.print(payload.substr(counter,end_index).c_str());
-        Serial.printf("sent %d bytes\n",sent);
-        counter = end_index;
+    while (counter < max_index)
+    {
+        size_t sent = client.print(payload.data() + counter);
+        Serial.printf("\nsent %d bytes\n", sent);
+        counter += sent;
         vTaskDelay(1);
     }
     result = client.endPublish();
-    if(!result){
+    if (!result)
+    {
         Serial.println("unable to end publish");
         return AWSErrors::PUBLISH_FAILED;
     }
     Serial.println("published successful");
     return AWSErrors::OK;
 }
-void AWSManager::messageHandler(char* topic, byte* payload, unsigned int length) {
+
+bool AWSManager::isConnected()
+{
+    return client.connected();
+}
+void AWSManager::messageHandler(char *topic, byte *payload, unsigned int length)
+{
     // Your message handling code here
     Serial.print("incoming: ");
     // Serial.println(topic);
--- lib/AWSManager/AWSManager.h
+++ lib/AWSManager/AWSManager.h
@@ -2,16 +2,16 @@
 #ifndef AWSMANAGER_H
 #define AWSMANAGER_H
 
-
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include "WiFiManager.h"
-#include "DB_Handler.h" 
+#include "DB_Handler.h"
 
-#define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
+#define AWS_IOT_PUBLISH_TOPIC "esp32/pub"
 #define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
-enum class AWSErrors{
+enum class AWSErrors
+{
     OK,
     CONNECTION_FAILED,
     DISCONNECTION_FAILED,
@@ -21,23 +21,22 @@ enum class AWSErrors{
     INVALID_ENDPOINT,
 };
 
-
-class AWSManager {
+class AWSManager
+{
 public:
-    AWSManager(WiFiManager* wifiManager);
+    AWSManager(WiFiManager *wifiManager);
     AWSErrors init();
     AWSErrors connect();
-    AWSErrors publish(std::string& topic, std::string& payload);
+    AWSErrors publish(std::string &topic, std::string payload);
+    bool isConnected();
+
 private:
-    WiFiManager* wifiManager;
+    WiFiManager *wifiManager;
     WiFiClientSecure net;
     PubSubClient client;
-    bool connected=false;
-    
+
     // Add any other necessary private members or methods here
-    static void messageHandler(char* topic, byte* payload, unsigned int length);
-    
+    static void messageHandler(char *topic, byte *payload, unsigned int length);
 };
 
 #endif
-
